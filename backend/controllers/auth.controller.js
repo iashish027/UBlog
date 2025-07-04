68f2431819bbd3c21fd7c6ee77c6241eeb9758fc
iashish027 <ashishch296@gmail.com>
2025-07-04 23:02:30 +0530

    add functionalities to sign up page

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -11,17 +11,30 @@ export const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
+    console.log("yes");
     return next(errorHandler(400, "all fields are required"));
   }
 
   try {
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+
+    console.log("new request");
+    if (existingUser) {
+      return next(errorHandler(400, "Username or Email already existss"));
+    }
     const newUser = new User({
       username,
       email,
       password,
     });
     await newUser.save();
+    return res.status(200).json({
+      success: false,
+      statusCode: 200,
+      message: "succefully saved user",
+    });
   } catch (err) {
+    console.log(err);
     next(err);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
-  const statusCode = err.statuscode || 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
   return res.status(statusCode).json({
     success: false,
