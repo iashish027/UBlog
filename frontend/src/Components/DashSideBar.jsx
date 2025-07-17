29c5a07e30d8d995131a7098346a9a4d596116ed
iashish027 <ashishch296@gmail.com>
2025-07-17 22:29:09 +0530

    add dashboard with dashboard sidebar

---
+++ frontend/src/Components/DashSideBar.jsx
@@ -0,0 +1,26 @@
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link } from "react-router-dom";
+
+export default function DashSideBar() {
+  return (
+    <div className="h-fit md:h-screen border-r shadow-md">
+      <div className="p-4 font-bold text-xl border-b">Dashboard</div>
+      <nav className="flex flex-col p-4 space-y-2">
+        <Link
+          to="/dashboard?tab=profile"
+          className="flex items-center gap-2 p-2 text-gray-700 rounded hover:bg-gray-100"
+        >
+          <HiUser className="w-5 h-5" />
+          <span>Profile</span>
+        </Link>
+        <button
+          className="flex items-center gap-2 p-2 text-red-600 rounded hover:bg-red-50"
+          onClick={() => console.log("Sign out logic")}
+        >
+          <HiArrowSmRight className="w-5 h-5" />
+          <span>Sign Out</span>
+        </button>
+      </nav>
+    </div>
+  );
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import DashSideBar from "../Components/DashSideBar";
+import DashProfile from "../Components/DashProfile";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    setTab(tabFromUrl);
+    console.log(tabFromUrl);
+  }, [location.search]);
+  return (
+    <div className="md:min-h-screen flex md:flex-row  flex-col">
+      <div className="w-full md:w-56 border-2 ">
+        {/* sidebar */}
+        <DashSideBar />
+      </div>
+      <div>
+        {/* profile... */}
+        {tab == "profile" && <DashProfile />}
+      </div>
+    </div>
+  );
 }
